3b988309da78cfec1fc4e8ddcbd48d0c83543e92
= <=>
2025-06-27 16:34:46 -0300

    Botão para segunda página

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'app/test.html')
+
+def dados(request):
+    return render(request, "app/dados.html")
